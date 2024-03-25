c36dd3d66f920314af615e6c266e4471f1e3f4fd
Marta <160023329+Marta-fn@users.noreply.github.com>
2024-03-25 12:45:35 +0000

    Updated JS
    • Added JS to the select element
    • Injected the time and date in HTML using JavaScript

--- src/index.js
+++ src/index.js
@@ -13,3 +13,33 @@ setInterval(function () {
   showTime("#berlin", "Europe/Berlin");
   showTime("#tokyo", "Asia/Tokyo");
 }, 1000);
+
+function inicialTime() {
+  let currentCityElement = document.querySelector("#currentCity");
+  let currentDateElement = currentCityElement.querySelector(".current-date");
+  let currentTimeElement = currentCityElement.querySelector(".current-time");
+  let currentCityTime = moment().tz("Europe/Lisbon");
+
+  currentDateElement.innerHTML = currentCityTime.format("dddd, MMMM DD, YYYY");
+  currentTimeElement.innerHTML = currentCityTime.format("HH:mm:ss");
+
+  let citiesSelect = document.querySelector("#optionCity");
+  citiesSelect.addEventListener("change", updateCity);
+}
+
+function updateCity(event) {
+  let optionCityTimeZone = event.target.value;
+  if (event.target.value.length > 0) {
+    let optionCityTime = moment().tz(optionCityTimeZone);
+    let currentCityElement = document.querySelector("#currentCity");
+    let currentDateElement = currentCityElement.querySelector(".current-date");
+    let currentTimeElement = currentCityElement.querySelector(".current-time");
+    currentDateElement.innerHTML = optionCityTime.format("dddd, MMMM DD, YYYY");
+    currentTimeElement.innerHTML = optionCityTime.format("HH:mm:ss");
+  } else {
+    inicialTime();
+  }
+}
+
+inicialTime();
+setInterval(inicialTime, 1000);
